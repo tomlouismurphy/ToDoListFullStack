90e33bd02209d916f54e9741adb36a3378b59d63
tom.louis.murphy <tom.louis.murphy@gmail.com>
2017-11-09 11:52:22 -0600

    to do list rendering, styling beyond that

--- reacttodo/src/TasksToDo/TasksToDo.js
+++ reacttodo/src/TasksToDo/TasksToDo.js
@@ -5,17 +5,35 @@ export class Tasks extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			tasks: this.props.tasks,
+			tasks: [],
 			selectedTask: ''
 		}
 	}
+	componentWillReceiveProps(nextprops) {
+		const obj = JSON.parse(nextprops.tasks);
+		const state = this.state;
+		for (let i = 0; i < obj.length; i++){
+			state.tasks.push(obj[i].task);
+		}
+		this.setState(state);
+		console.log(this.state.tasks);
+	}
 	render() {
-		const list = this.props.tasks.map((item, i) => {
-			return <div key={i}>{item.task}</div>
-		})
-		console.log(list);
-		return(
-			<p>hi</p>
-		)
+		if (this.state.tasks.length > 0){
+			console.log(this.state.tasks);
+			console.log(this.props.taskMoreDetail);
+			const list = this.state.tasks.map((item, i) => {
+				return <div key={i} onClick={this.props.taskMoreDetail}>{item}</div>
+			})
+			return(
+				<div>
+					{list}
+				</div>
+			)
+		} else {
+			return(
+				<p>eh</p>
+			)
+		}
 	}
 }
--- reacttodo/src/ToDo/ToDo.js
+++ reacttodo/src/ToDo/ToDo.js
@@ -13,18 +13,28 @@ export class ToDo extends Component {
 		}
 	}
 	taskMoreDetail = (e) => {
+		console.log(e.target.innerText);
+		console.log(this.state.tasks);
 		const state = this.state;
 		for (let i = 0; i < this.state.tasks.length; i++){
 			if (e.target.innerText === this.state.tasks[i].task){
 				state.selectedTask = this.state.tasks[i];
+				console.log(state.selectedTask);
+			} else {
+				console.log('fail');
 			}
 		}
+		this.setState(state);
+	}
+	componentWillReceiveProps(nextprops) {
+		const state = this.state;
+		state.tasks = nextprops.tasks;
 		this.setState(state);
 	}
 	render() {
 		return(
 			<div>
-				<Tasks tasks={this.state.tasks}/>
+				<Tasks tasks={this.state.tasks} taskMoreDetail={this.taskMoreDetail}/>
 				<Details tasks={this.state.tasks} selectedTask={this.state.selectedTask}/>
 				<Create/>
 			</div>
